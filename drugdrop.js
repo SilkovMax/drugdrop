ea39a4c52b17f75bec62ade682ea645a1a214e0a
Maksim Silkov <silkov34@yandex.ru>
2019-08-03 17:24:57 +0300

    hm

--- drugdrop.js
+++ drugdrop.js
@@ -8,6 +8,8 @@ let left = 5;
 //let div1_left = 0;
 //let dist = 0;
 let curleft = 0;
+let divLeft = 0;
+let offsetcanvasDiv = 0;
 
 let createElements = function() {
 	canvas = document.createElement("div");
@@ -19,27 +21,31 @@ let createElements = function() {
 		div.classList.add("rect");
 		div.setAttribute("id", "carret");
 		canvas.appendChild(div);
+		offsetcanvasDiv = div.offsetLeft - canvas.offsetLeft;
 	}
 }
 
 window.onkeydown = function move_left(){
 	let x=canvas.offsetLeft;
 	let y=div.offsetLeft;
+	let z=y-x;
+	
+	
 	console.log(x);
 	console.log(y);
-	if(event.keyCode==37){
-		if (y>x)
-			div.style.left = y - left + 'px';
-		else 
-			div.style.left = x + 'px';
+	if(event.keyCode==37){//левая
+		divLeft=divLeft - left;
+		if (Math.abs(divLeft) > offsetcanvasDiv - 3)
+			divLeft = -offsetcanvasDiv + 3;		
+			div.style.left = divLeft + 'px';
 	}
 	else 
-		if(event.keyCode==39){
+		if(event.keyCode==39){//правая
 			if (y+div.offsetWidth<x+canvas.offsetWidth){	
-				div.style.left = y + div.offsetWidth + left + 'px';
+				div.style.left = y + left + 'px';
 			}
 			else 
-				div.style.left = x + canvas.offsetWidth + 'px';
+				div.style.left = x + canvas.offsetWidth - div.offsetWidth + 'px';
 		}
 };
 /*
